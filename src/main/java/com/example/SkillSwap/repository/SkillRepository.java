6e151e1f4c0f9d413941ca4634ac6f28db53aaf8
Jeslyn Lai <imlyj5@gmail.com>
2025-07-30 10:51:09 -0700

    Fix merge conflict in SkillRepository.java

--- src/main/java/com/example/SkillSwap/repository/SkillRepository.java
+++ src/main/java/com/example/SkillSwap/repository/SkillRepository.java
@@ -1,16 +1,10 @@
 package com.example.SkillSwap.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.SkillSwap.model.Skill;
 
-interface SkillRepository extends JpaRepository<Skill, Long> {
-
-<<<<<<< HEAD
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.SkillSwap.model.Skill;
 
 @Repository
 public interface SkillRepository extends JpaRepository<Skill, Long> {
-=======
->>>>>>> 4ad042c1596958b2c000c2040f25ba7460ea94a4
+    // Basic CRUD operations are inherited from JpaRepository
 }
